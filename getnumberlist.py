bbd3a24873fa2831d3c799161112f6fbc42696f6
ngerll <ngerll@yeah.net>
2016-02-15 11:39:56 +0800

    0.2

--- getnumberlist.py
+++ getnumberlist.py
@@ -11,7 +11,7 @@ reload(sys)
 sys.setdefaultencoding('utf-8')
 
 head = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.101 Safari/537.36',
-        'Cookie':'StaffLoginInfo=LILP60; cklflag=1; ckpcode=71; cksid=LILP60; BIGipServerWEG-WEBAPP2=1795499396.41030.0000; BIGipServerWEG-WEBAPP1=1694836100.25675.0000; CNZZDATA1257374541=828476400-1454298228-http%253A%252F%252Fwww.woego.cn%252F%7C1454298228; WOEGO_JSESSIONID=219D7832346C4B66964EBD75691A4D0D'}
+        'Cookie':'StaffLoginInfo=LILP60; cklflag=1; ckpcode=71; cksid=LILP60; Hm_lvt_e134fee1edb436d9a4b58261f92fdeb8=1452216852,1452217227,1452218466; BIGipServerWEG-WEBAPP2=1761944964.41030.0000; BIGipServerWEG-WEBAPP1=1694836100.25675.0000; WOEGO_JSESSIONID=D18B888363804904BE6C86D7F6835DDF'}
 
 
 def getboxlist(orderid,chnlid,chnlname,areacode):
@@ -76,12 +76,13 @@ def getnumberlist(boxlist):
                     list =  "insert into huqiao_order values  ('%s','%s','%s','%s','%s','%s','%s',to_date('%s','yyyy/mm/dd,hh24:mi:ss'),'%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s');" % (str(orderid),str(sorderid),str(productid),str(serialnumber),str(productid),str(paymoney),'50',str(paytime),str(chnlid),areacode,'0','','','900',str(orderid),productname,str(packageno),str(int(paymoney)*100),'5000',str(5000-int(paymoney)*100),'0') \
                             + "\n" + "commit;"
                     file.write(list + '\n')
+                    print '第%d个卡包中的第%d个号码已生成插入脚本，并存入文件%s中' % (i+1,j+1,str(file.name))
 
 
 if __name__ == '__main__':
-    orderid = '7116020109822640'
-    chnlid = 'YMZHL'
-    chnlname = '孝南毛陈杨爱莲MINI'
+    orderid = '7116021409854015'
+    chnlid = 'YN0JU'
+    chnlname = '孝南三汊徐艳MINI（异网分销渠道）'
     areacode = '0712'
     getnumberlist(getboxlist(orderid,chnlid,chnlname,areacode))
 
